eafcb33a0f71dd244d4e1fa8c19bfc974bd9d385
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-04-17 10:36:39 -0600

    Add comment describing class per rubocop

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,6 +2,8 @@
 
 require 'pry-byebug' # temporary for debugging
 
+# This is the player class, it will hold the symbol, a boolean for whether it is their turn
+# and their most recent play
 class Player
   def initialize(player_symbol, turn, play)
     @player_symbol = player_symbol
